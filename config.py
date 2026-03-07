10e4c6bd28fbadf691d0e3cb8e94d6fbac9cd015
SOUL々H҉A҉C҉K҉E҉R҉ <63580147+dqanshi@users.noreply.github.com>
2026-03-07 17:26:15 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+BOT_TOKEN = 8718879180:AAEoHn17-OfJcOvkIB2TzC154aYzzPldYAk
+
+OWNER_ID = 123456789
+
+DOWNLOAD_DIR = "downloads"
+LOG_DIR = "logs"
+
+CLICKHOUSE_HOST = "localhost"
+CLICKHOUSE_DB = "telegram"
+
+BATCH_SIZE = 5000
+STATE_FILE = "import_state.json"
