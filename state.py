2d502700e6d784ab20dab4abdfa922abec5f35c3
SOUL々H҉A҉C҉K҉E҉R҉ <63580147+dqanshi@users.noreply.github.com>
2026-03-07 17:27:15 +0530

    Create state.py

---
+++ state.py
@@ -0,0 +1,18 @@
+import json
+import os
+from config import STATE_FILE
+
+def save_state(line):
+
+    with open(STATE_FILE,"w") as f:
+        json.dump({"line":line},f)
+
+def load_state():
+
+    if not os.path.exists(STATE_FILE):
+        return 0
+
+    with open(STATE_FILE) as f:
+        data=json.load(f)
+
+    return data["line"]
