b57d90bf6b7960025bbd4e30f2e1089e64a2d809
SOUL々H҉A҉C҉K҉E҉R҉ <63580147+dqanshi@users.noreply.github.com>
2026-03-08 09:36:59 +0530

    Update extractor.py

--- extractor.py
+++ extractor.py
@@ -1,34 +1,15 @@
 import subprocess
 import os
 
-
-# -------------------------
-# STREAM EXTRACTION (FAST)
-# -------------------------
-
 def extract_stream(archive, password=""):
 
-    # find internal file name inside archive
-    list_cmd = ["7z", "l", archive]
+    # detect file name automatically
+    filename = os.path.basename(archive)
 
-    result = subprocess.run(
-        list_cmd,
-        capture_output=True,
-        text=True
-    )
-
-    lines = result.stdout.splitlines()
-
-    target_file = None
-
-    for l in lines:
-
-        if ".txt" in l or ".csv" in l:
-            target_file = l.split()[-1]
-            break
-
-    if not target_file:
-        raise Exception("No txt/csv file inside archive")
+    if ".txt.7z" in filename:
+        target_file = filename.replace(".7z.001", "").replace(".7z", "")
+    else:
+        target_file = "telegram.txt"
 
     cmd = [
         "7z",
@@ -56,10 +37,6 @@ def extract_stream(archive, password=""):
     return process
 
 
-# -------------------------
-# DISK EXTRACTION (FALLBACK)
-# -------------------------
-
 def extract_to_disk(archive, password=""):
 
     output_dir = "downloads/extracted"
@@ -79,11 +56,8 @@ def extract_to_disk(archive, password=""):
     if password:
         cmd.append(f"-p{password}")
 
-    print("Running:", " ".join(cmd))
-
     subprocess.run(cmd, check=True)
 
-    # find extracted txt/csv file
     for root, dirs, files in os.walk(output_dir):
 
         for f in files:
