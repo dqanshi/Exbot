b011076d1c83387d06f3646bbe5d2b06e2333f18
SOUL々H҉A҉C҉K҉E҉R҉ <63580147+dqanshi@users.noreply.github.com>
2026-03-08 10:21:17 +0530

    Update parser.py

--- parser.py
+++ parser.py
@@ -10,57 +10,15 @@ def parse_line(line):
     if len(parts) < 2:
         return None
 
-    # user id
     try:
-        user_id = int(parts[0])
+        user_id = int(float(parts[0]))
     except:
-        try:
-            user_id = int(float(parts[0]))
-        except:
-            return None
+        return None
 
-    username = ""
-    first_name = ""
-    last_name = ""
-    phone = ""
-    status = ""
-
-    linked_usernames = []
-    extra_ids = []
-
-    if len(parts) > 1:
-        username = parts[1]
-
-    if len(parts) > 2:
-        first_name = parts[2]
-
-    if len(parts) > 3:
-        last_name = parts[3]
-
-    for p in parts:
-
-        if p.startswith("@"):
-            linked_usernames.append(p)
-
-        elif p.isdigit():
-
-            if len(p) > 9:
-                extra_ids.append(int(p))
-
-            elif len(p) >= 10 and len(p) <= 15:
-                phone = p
-
-        elif "online" in p or "recent" in p:
-            status = p
+    username = parts[1]
 
     return (
         user_id,
         username,
-        first_name,
-        last_name,
-        phone,
-        status,
-        linked_usernames,
-        extra_ids,
         line
     )
